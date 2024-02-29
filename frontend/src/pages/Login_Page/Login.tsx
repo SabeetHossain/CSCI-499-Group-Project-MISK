b46ad356497b18898e8798618209fd3e615dd378
Sabeet Hossain <sabeethossain11@gmail.com>
2024-02-28 20:15:13 -0500

    testing some login

--- frontend/src/pages/Login_Page/Login.tsx
+++ frontend/src/pages/Login_Page/Login.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import "./Login.css";
 
-const Login = () => {
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
   return (
-    //placeholder code/testing
-    <div>you are on the login page</div>
-  );
+  <div>
+      <div className="login-page">
+        <h1>MISK</h1>
+        <div className="signin-container">
+          <div>
+            <h2>Sign In</h2>
+            <div>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="username"
+              />
+            </div>
+            <div>
+              <button>sign in</button>
+            </div>
+            <div>
+              <Link to="/Register">Or Sign Up Here!</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   );
 };
 
 export default Login;
