563aca4029e805f4cea2d391d1fe7801f64bb41d
Sabeet Hossain <sabeethossain11@gmail.com>
2024-02-28 20:39:14 -0500

    update frontend register

--- frontend/src/pages/Register_Page/Register.tsx
+++ frontend/src/pages/Register_Page/Register.tsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Register.css";
 
-const Register = () => {
+function Register() {
+  const [username, setUsername] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [confirmPassword, setConfirmPassword] = useState("");
+
+
+  const onSubmitForm = async e => {
+      e.preventDefault(); //prevents refresh on submit
+       console.log("submitted");
+    }
+
+
   return (
-    //placeholder code/testing
-    <div>you are on the sign up page</div>
+    <div>
+      <div className="signup-page">
+        <header>
+          <h1>MISK</h1>
+        </header>
+        <div className="signup-container">
+          <div>
+            <h2>Sign Up</h2>
+            <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+                <input type = "text" className="form-control" value ={username}
+                onChange={e => setUsername(e.target.value)}
+                placeholder="Enter Username"
+                />
+                <input type="submit" value="Register" />
+            </form>
+            <div>
+              <Link to="/Login">
+                Already have an account? Sign In Here
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
