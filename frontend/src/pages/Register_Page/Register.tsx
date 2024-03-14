fa6a6f6ca64a78a803d6da6ccffe99f2eecacd85
Ian Clarke <ianisian2003@gmail.com>
2024-03-13 23:06:19 -0400

    updated Register.tsx: organized code and improved client side layout

--- frontend/src/pages/Register_Page/Register.tsx
+++ frontend/src/pages/Register_Page/Register.tsx
@@ -23,35 +23,50 @@ function Register() {
     }
   };
 
-  return (
-    <Fragment>
+return (
+  <Fragment>
+    <div className="container">
       <h1 className="text-center mt-5">MISK</h1>
-      <h2 className="text-center mt-5">Sign Up</h2>
-      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
-        <input
-          type="text"
-          className="form-control"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-          placeholder="Enter Username"
-        />
-        <input
-          type="text"
-          className="form-control"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          placeholder="Enter Password"
-        />
-        <input
-          type="text"
-          className="form-control"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          placeholder="Enter Email"
-        />
-        <button type="submit" className="btn btn-success">Register</button>
-      </form>
-    </Fragment>
+      <div className="card mt-5">
+        <div className="card-body">
+          <h2 className="text-center mb-4">Sign Up</h2>
+          <form onSubmit={onSubmitForm}>
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                placeholder="Enter Username"
+               />
+            </div>
+            <div className="mb-3">
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                placeholder="Enter Password"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                placeholder="Enter Email"
+              />
+            </div>
+            <button className="btn btn-primary btn-block">Register</button>
+          </form>
+        </div>
+      </div>
+      <div className="text-center mt-3">
+        Already have an account? <Link to="/login">Login</Link>
+      </div>
+    </div>
+  </Fragment>
   );
 }
 
