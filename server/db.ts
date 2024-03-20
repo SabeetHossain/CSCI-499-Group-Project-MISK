f831b894645b54724065c85062a23e26c967c36a
Ian Clarke <ianisian2003@gmail.com>
2024-03-20 18:38:20 -0400

    Update database connection for Heroku deployment

--- server/db.ts
+++ server/db.ts
@@ -22,3 +22,6 @@ const pool = new Pool({
 });
 
 export default pool;
+
+
+//DATABASE_URL: postgres://u94q2hmdkutfig:p93efda47086cc7c59d40f6f5e8d471f74d8095429e6e641c008fecfc8b26b046@ceu9lmqblp8t3q.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d7uuks355v9e6q
