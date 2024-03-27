71d3aadd2b06cb5abb6daa38bd6f91f4dfa23810
Ian Clarke <ianisian2003@gmail.com>
2024-03-27 18:21:46 -0400

    added further functionality

--- frontend/src/pages/Register_Page/Register.tsx
+++ frontend/src/pages/Register_Page/Register.tsx
@@ -5,7 +5,8 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+import { Link } from "react-router-dom";
+//import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -114,7 +115,8 @@ function SignUp() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
+                {/* <Link href="#" variant="body2"> */}
+                <Link to='/Login'>
                   Already have an account? Sign in
                 </Link>
               </Grid>
