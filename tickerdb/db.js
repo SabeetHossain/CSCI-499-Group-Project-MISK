2464217cce4894fac22e9fe19be23071b1d3464d
Kevin Wander <87961130+Kwander@users.noreply.github.com>
2024-05-08 23:36:12 -0400

    Added search bar functionality

--- tickerdb/db.js
+++ tickerdb/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "turtle1357",
+    password: "post123",
     host: "localhost",
     port: 5432,
     database: "tickers"
